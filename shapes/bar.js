37a37c19a4ba43ed57e12563f1836a18d0e18d5f
Midas van Soest <midasvsoest@gmail.com>
2025-04-14 16:36:37 +0200

    Create player lives, damage flash and damage invincibility. CubeRain empties array after finnishing

--- shapes/bar.js
+++ shapes/bar.js
@@ -1,5 +1,8 @@
 class Bar {
     constructor(){
+        this.width = 6;
+        this.height = 6;
+        
         const container = document.getElementById("game-area");
         this.gameWidth = container.clientWidth;
         this.gameHeight = container.clientHeight;
@@ -8,6 +11,24 @@ class Bar {
         this.positionY = this.gameHeight;
 
         this.createDomElement(container);
+        this.updateUI();
+    }
+
+    updateUI() {
+        this.barElm.style.left = this.positionX + "px";
+        this.barElm.style.bottom = this.positionY + "px";
+        this.barElm.style.width = this.width + "px";
+        this.barElm.style.height = this.height + "px";
+    }
+
+    createDomElement(container) {
+        this.barElm = document.createElement("div");
+        this.barElm.className = "bar";
+        container.appendChild(this.barElm);
+    }
+
+    moveDown() {
+        this.positionY -= this.speed;
         this.updateUI();
     }
 }
--- shapes/cubeRain.js
+++ shapes/cubeRain.js
@@ -1,8 +1,8 @@
 class CubeRain {
-    constructor() {
-        this.width = 6;
-        this.height = 6;
-        this.speed = 1;
+    constructor(width, height, speed) {
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
 
         const container = document.getElementById("game-area");
         this.gameWidth = container.clientWidth;
@@ -13,6 +13,8 @@ class CubeRain {
 
         this.createDomElement(container);
         this.updateUI();
+
+        this.projectile;
     }
 
     updateUI() {
@@ -21,7 +23,6 @@ class CubeRain {
         this.cubeRainElm.style.width = this.width + "px";
         this.cubeRainElm.style.height = this.height + "px";
     }
-
 
     createDomElement(container) {
         this.cubeRainElm = document.createElement("div");
@@ -33,4 +34,66 @@ class CubeRain {
         this.positionY -= this.speed;
         this.updateUI();
     }
+}
+
+const projectileArr = [];
+setInterval(() => {
+    console.log(projectileArr)
+}, 3000)
+
+function cubeRain(width, height, speed, activeTime, spawnRate) {
+    let spawningActive = true;
+
+    const spawnInterval = setInterval(() => {
+        const newProjectile = new CubeRain(width, height, speed);
+        projectileArr.push(newProjectile);
+    }, spawnRate);
+
+    setTimeout(() => {
+        clearInterval(spawnInterval);
+        spawningActive = false;
+    }, activeTime);
+
+    const moveInterval = setInterval(() => {
+        for (let i = projectileArr.length - 1; i >= 0; i--) {
+            const projectile = projectileArr[i];
+            projectile.moveDown();
+
+            const left = parseInt(projectile.cubeRainElm.style.left, 10);
+            const bottom = parseInt(projectile.cubeRainElm.style.bottom, 10);
+
+            if (
+                left < 0 ||
+                bottom < 0 ||
+                left > this.gameWidth ||
+                bottom > this.gameHeight
+            ) {
+                projectile.cubeRainElm.remove();
+                projectileArr.splice(i, 1);
+            }
+        }
+
+        if (!spawningActive && projectileArr.length === 0) {
+            clearInterval(moveInterval);
+        }
+    }, 20);
+
+    const collisionManager = new CollisionManager(player, projectileArr, () => {
+        player.lives--;
+        player.updateLivesUI();
+
+        if (player.lives <= 0) {
+            clearInterval(cubeRain);
+            for (let i = projectileArr.length - 1; i >= 0; i--) {
+                projectileArr.pop();
+            }
+        } else {
+            console.log("Hit! Lives left:", player.lives);
+        }
+    });
+
+    setInterval(() => {
+        projectileArr.forEach(p => p.moveDown());
+        collisionManager.checkCollisions();
+    }, 1);
 }
